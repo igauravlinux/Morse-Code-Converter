acbf1caf11f665531caaccef1b4bd3afdbffe9bc
gaurav <171420238+igauravraut@users.noreply.github.com>
2026-04-06 01:45:35 +0530

    Project

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,12 +1,39 @@
 import java.util.Scanner;
 public class Main{
+
+        static void textToMorse(char arr[] , String morse[] , String s) {
+            //      here I'm using linear search for every letters
+            for(int i = 0; i < s.length(); i++){
+
+                boolean found = false;
+                char key = s.charAt(i);
+
+                if(key == ' '){
+                    System.out.print(" / ");
+                    continue;
+                }
+
+                for(int j = 0; j < arr.length; j++){
+                    if(arr[j] == key){
+
+                        System.out.print(morse[j] +" ");
+                        found = true;
+                        break;
+                    }
+                }
+                if(!found){
+                    System.out.print("Not found");
+                }
+            }
+
+        }
+
+        static void morseToText(){
+
+        }
+
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-
-//        Taking text input from user
-        String str;
-        System.out.print("Type text to convert into morse code: ");
-        str = sc.nextLine().toUpperCase();
 
 //        letters array
         char arr[] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
@@ -15,29 +42,44 @@ public class Main{
         String morse[] = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
 
 
-//      here I'm using linear search for every letters
-        for(int i = 0; i < str.length(); i++){
+        int choice = 0;
 
-            boolean found = false;
-            char key = str.charAt(i);
+        while(choice != 3){
+            System.out.println();
+            System.out.println("--------------------------------------------");
+            System.out.println("1. Text to Morse");
+            System.out.println("2. Morse to Text");
+            System.out.println("3. Exit");
+            System.out.print("Choose a number between (1 to 3): ");
+            choice = sc.nextInt();
+            sc.nextLine();
 
-            if(key == ' '){
-                System.out.print("/");
-                continue;
-            }
-
-            for(int j = 0; j < arr.length; j++){
-                if(arr[j] == key){
-
-                    System.out.print(morse[j] +" ");
-                    found = true;
+            switch (choice){
+                case 1:
+                    System.out.print("Enter text: ");
+                    String str = sc.nextLine().toUpperCase();
+                    textToMorse(arr,morse,str);
+                    System.out.println();
                     break;
-                }
+
+                case 2:
+                    System.out.println("Enter morse: ");
+                    String morseInput = sc.nextLine();
+//                    morse to call
+                    break;
+
+                case 3:
+                    System.out.println("Exiting");
+                    break;
+
+                default:
+                    System.out.println("Invalid Choice");
+
+
             }
-                if(!found){
-                    System.out.println("Not found");
-                }
         }
+
+
 
 
 
